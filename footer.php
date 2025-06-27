05063f2abf392265e42da3d75c91e81a5a841753
Min Thiha Tun <minthihatun@gmail.com>
2025-06-27 17:17:18 +0630

    Footer Page

---
+++ footer.php
@@ -0,0 +1,65 @@
+<style>
+    .myFooter {
+      background-color: #343a40;
+      padding: 40px 0;
+      color: white;
+    }
+    .myFooter h5 {
+      margin-bottom: 20px;
+      font-weight: bold;
+    }
+    .myFooter a {
+      display: block;
+      color: #ccc;
+      text-decoration: none;
+      margin: 8px 0;
+    }
+    .myFooter a:hover {
+      color: white;
+      text-decoration: underline;
+    }
+</style>
+
+
+
+<footer class="myFooter">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 text-center">
+        <h5>Latest News</h5>
+        <a href="#">News Item 1</a>
+        <a href="#">News Item 2</a>
+        <a href="#">News Item 3</a>
+        <a href="#">News Item 4</a>
+        <a href="#">News Item 5</a>
+      </div>
+      <div class="col-md-3 text-center">
+        <h5>Latest News</h5>
+        <a href="#">News Item 1</a>
+        <a href="#">News Item 2</a>
+        <a href="#">News Item 3</a>
+        <a href="#">News Item 4</a>
+        <a href="#">News Item 5</a>
+      </div>
+      <div class="col-md-3 text-center">
+        <h5>Latest News</h5>
+        <a href="#">News Item 1</a>
+        <a href="#">News Item 2</a>
+        <a href="#">News Item 3</a>
+        <a href="#">News Item 4</a>
+        <a href="#">News Item 5</a>
+      </div>
+      <div class="col-md-3 text-center">
+        <h5>Latest News</h5>
+        <a href="#">News Item 1</a>
+        <a href="#">News Item 2</a>
+        <a href="#">News Item 3</a>
+        <a href="#">News Item 4</a>
+        <a href="#">News Item 5</a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
