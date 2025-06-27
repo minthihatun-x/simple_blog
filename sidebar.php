3ddf8fda7dbcd8f840847f2f889ba4e786f55125
Min Thiha Tun <minthihatun@gmail.com>
2025-06-27 17:27:39 +0630

    Sidebar Page

---
+++ sidebar.php
@@ -0,0 +1,24 @@
+<style>
+    .list-group-item {
+      margin-bottom: 15px;
+      border-radius: 10px;
+    }
+    .list-group-item > a {
+        font-family: 'Inter', sans-serif;
+    }
+    .list-group-item > a:hover {
+        color: #000;
+      text-decoration: none;
+
+    }
+</style>
+
+<aside class="col-md-3">
+    <ul class="list-group">
+        <li class="list-group-item"><a href="admin.php">Post Insert</a></li>
+        <li class="list-group-item"><a href="show_all_post.php">Show All Posts</a></li>
+        <li class="list-group-item"><a href="">Testing Three</a></li>
+        <li class="list-group-item"><a href="">Testing Four</a></li>
+        <li class="list-group-item"><a href="">Testing Five</a></li>
+    </ul>
+</aside>
